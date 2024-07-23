88055ff56377806ceef7ca7c6552af07405620df
Aris Aditya Nugraha <63295820+ArisAdityaNugraha@users.noreply.github.com>
2024-07-23 16:37:44 +0700

    Add files via upload

---
+++ resources/views/show.blade.php
@@ -0,0 +1,40 @@
+@extends('products.layout')
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2> Show Product</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+            </div>
+        </div>
+    </div>
+   
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>nama_produk:</strong>
+                {{ $product->nama_produk }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>foto:</strong>
+                {{ $product->foto }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>harga:</strong>
+                {{ $product->harga }}
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Details:</strong>
+                {{ $product->desc_produk }}
+            </div>
+        </div>
+    </div>
+@endsection
